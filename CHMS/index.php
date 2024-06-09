d7acf2cc7da8716cb73a8afb1ca4c0347e55b826
Geejeee <156183628+Geeejeee@users.noreply.github.com>
2024-06-09 22:12:25 +0800

    Add files via upload

---
+++ CHMS/index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="css/style.css" />
+    <meta charset="utf-88">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> CHMS </title>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="landing-page">
+        <div class="head">
+            <img class="landingpagephoto" alt="" src="images/hospital1.png">
+            <div class="title">
+                <div class="content">
+                    <h1><b>Carehub HMS</b></h1>
+                </div>
+                <div class="content2">
+                    <form action="#" method="get"> <button class="order-now-btn" type="submit">Book an Appointment</button></form>
+                </div>
+            </div>
+        </div>
+
+        <header>
+            <a href="#" class="logo">
+                <img src="images/chww.png" alt="Logo" class="img-fluid" style="max-width: 100px;">
+                <span>Carehub</span>
+            </a>
+            <ul class="navbar">
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+            <div class="main">
+                <a href="#" class="user">Book an Appointment</a>
+                <div class="bx bx-menu" id="menu-icon"></div>
+            </div>
+        </header>
+
+        <div class="ltext">
+            <h5>Logins</h5>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <img src="images/patient.png">
+                <div class="card-content">
+                    <h3>Patient</h3>
+                    <a href="" class="btn">Login</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/doctor.png">
+                <div class="card-content">
+                    <h3>Doctor</h3>
+                    <a href="" class="btn">Login</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/admin.png">
+                <div class="card-content">
+                    <h3>Admin</h3>
+                    <a href="" class="btn">Login</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="group">
+                <div class="overlap-group">
+                    <div class="text-wrapper">Our Services</div>
+                    <p class="div">Take a look at some of our key services</p>
+                </div>
+                <div class="services-grid">
+                    <div class="service-item">
+                        <img class="service-icon" src="images/material-symbols_cardiology.png" alt="Cardiology" />
+                        <div class="service-text">Cardiology</div>
+                    </div>
+                    <div class="service-item">
+                        <img class="service-icon" src="images/fa-solid_ribbon.png" alt="Orthopaedic" />
+                        <div class="service-text">Orthopaedic</div>
+                    </div>
+                    <div class="service-item">
+                        <img class="service-icon" src="images/ph_eye-duotone.png" alt="Ophthalmology" />
+                        <div class="service-text">Ophthalmology</div>
+                    </div>
+                    <div class="service-item">
+                        <img class="service-icon" src="images/fontisto_like.png" alt="High Quality" />
+                        <div class="service-text">High Quality</div>
+                    </div>
+                    <div class="service-item">
+                        <img class="service-icon" src="images/mdi_medicine-bottle.png" alt="Pharma" />
+                        <div class="service-text">Pharma</div>
+                    </div>
+                    <div class="service-item">
+                        <img class="service-icon" src="images/material-symbols_neurology-outline.png" alt="Neurologist" />
+                        <div class="service-text">Neurologist</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="box1">
+            <div class="group1">
+                <div class="overlap1">
+                    <div class="overlap-group-wrapper1">
+                        <div class="overlap-group1">
+                            <div class="text-wrapper1">Who We Are</div>
+                            <div class="div1">Getting to know Us!</div>
+                        </div>
+                    </div>
+                    <p class="p1">
+                        The Carehub Hospital Management System (CHMS) is designed for Any Hospital to replace their existing manual,
+                        paper based system. The new system is to control the following information; patient information, room
+                        availability, staff and operating room schedules, and patient invoices. These services are to be provided in
+                        an efficient, cost effective manner, with the goal of reducing the time and resources currently required for
+                        such tasks.
+                    </p>
+                    <img class="carehub" src="images/chw3.png" />
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div class="contactus">
+                <div class="innerwrap">
+                    <section class="section1 clearfix">
+                        <div class="textcenter">
+                            <span class="shtext">Contact Us</span>
+                            <span class="seperator"></span>
+                            <h1>Drop Us a Mail!</h1>
+                        </div>
+                    </section>
+                    <section class="section2 clearfix">
+                        <div class="col2 column1 first">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126310.23771902619!2d124.5503246735233!3d8.382478804305922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32fff2d9810e9e81%3A0x2edcd5b84a6de2a2!2sCagayan%20de%20Oro%2C%20Misamis%20Oriental!5e0!3m2!1sen!2sph!4v1717770273446!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                        <div class="col2 column2 last">
+                            <div class="sec2contactform">
+                                <h3 class="sec2frmtitle">Want to Know More?? Drop Us a Mail</h3>
+                                <form action="">
+                                    <div class="clearfix">
+                                        <input class="col2 first" type="text" placeholder="FirstName">
+                                        <input class="col2 last" type="text" placeholder="LastName">
+                                    </div>
+                                    <div class="clearfix">
+                                        <input class="col2 first" type="Email" placeholder="Email">
+                                        <input class="col2 last" type="text" placeholder="Contact Number">
+                                    </div>
+                                    <div class="clearfix">
+                                        <textarea name="textarea" id="" cols="30" rows="7">Your message here...</textarea>
+                                    </div>
+                                    <div class="clearfix"><input type="submit" value="Send"></div>
+                                </form>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="footer">
+        <!-- Footer content -->
+        <footer class="footer">
+            <div class="footer__addr">
+                <img class="imgf" src="images/chw4.png">
+            </div>
+            <div class="footer__content">
+                <div class="nav__item">
+                    <h2 class="nav__title">About Us</h2>
+                    <ul class="nav__ul">
+                        <li>
+                            <h5 class="text">The Carehub Hospital Management System (CHMS) is designed for Any Hospital to replace their existing manual, paper based system. The new system is to control the following information</h5>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="nav__item">
+                    <h2 class="nav__title">Useful Links</h2>
+                    <ul class="nav__ul">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+
+                <div class="nav__item2">
+                    <h2 class="nav__title">Contact Us</h2>
+                    <ul class="nav__ul">
+                        <li><a>D-204, Hole Town South West, Philippines</a></li>
+                        <li><a>Phone: 11234456890</a></li>
+                        <li><a>Email: chms@gmail.com</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="legal">
+                <p>&copy; 2024 IT2R6. All rights reserved.</p>
+            </div>
+        </footer>
+    </div>
+
+
+    <!--js link--->
+    <script type="text/javascript" src="js/script.js"></script>
+
+</body>
+
+</html>
